e109f894184b625eb3574381e932f76ac6fe495a
ada popovic <popowucz.adrianna@gmail.com>
2020-05-03 14:02:50 +0200

    --

---
+++ T5/working shake/script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+
+    //create a new instance of shake.js.
+    var myShakeEvent = new Shake({
+        threshold: 15
+    });
+
+    // start listening to device motion
+    myShakeEvent.start();
+
+    // register a shake event
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    //shake event callback
+    function shakeEventDidOccur () {
+      $(".background").css('background-color','blue');
+
+    }
+};
