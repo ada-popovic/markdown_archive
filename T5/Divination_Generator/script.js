d5c862400411cb215dabf53dc5153cbaf4143643
ada popovic <popowucz.adrianna@gmail.com>
2020-05-11 17:44:07 +0200

    --

--- T5/Divination_Generator/script.js
+++ T5/Divination_Generator/script.js
@@ -22,52 +22,27 @@ window.onload = function() {
     //shake event callback
     function shakeEventDidOccur () {
 
+      function testDeviceOrientation() {
         // feature detect
         if (typeof DeviceMotionEvent.requestPermission === 'function') {
           DeviceMotionEvent.requestPermission()
             .then(permissionState => {
               if (permissionState === 'granted') {
-                  location.reload();
+                window.addEventListener('devicemotion', () => {});
               }
             })
             .catch(console.error);
         } else {
           // handle regular non iOS 13+ devices
         }
-
+      }
 
         //put your own code here etc.
         // alert('snow!');
 
-
-
+        location.reload();
+      
     }
-
-
-
-    // function shakeEventDidOccur () {
-    //
-    //   function testDeviceOrientation() {
-    //     // feature detect
-    //     if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    //       DeviceMotionEvent.requestPermission()
-    //         .then(permissionState => {
-    //           if (permissionState === 'granted') {
-    //             window.addEventListener('devicemotion', () => {});
-    //           }
-    //         })
-    //         .catch(console.error);
-    //     } else {
-    //       // handle regular non iOS 13+ devices
-    //     }
-    //   }
-    //
-    //     //put your own code here etc.
-    //     // alert('snow!');
-    //
-    //     location.reload();
-    //
-    // }
 };
 
 var myArray1 = [
